174ec5efb6ed169bfa92243c4468956e3489a6fd
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2023-10-12 14:40:18 -0700

    refactor: program flow - cleaned up code and specified properties more clearly

--- my_player3.py
+++ my_player3.py
@@ -1,51 +1,61 @@
 import math, random
 
 class MonteCarloTreeSearch:
-    class _BoardNode:
-        def __init__(self, player, board, parent=None):
+    class _Node:
+        def __init__(self, player, board, parent, is_root=False):
             self.player = player
             self.board = board
-            self.identifier = str(board)
             self.parent = parent
             self.children = []
-            self.num_children_expanded = 0
-            self.is_explored = False
+            self.num_children_simulated = 0
+            self.is_root = is_root
+            self.simulated = False
             self.simulation_visits = 0
             self.winning_simulation_visits = 0
             
     def __init__(self, input_file_path):
         self._input_file_path = input_file_path
-        self._expanded_nodes = {}
+        self._root_node = None
+        
+    # sets the root node for the MCTS tree
+    def set_root_node(self, player, current_board, previous_board):
+        self._root_node = self._Node(player, current_board, previous_board, is_root=True)
     
-    # selects path, starting from root node and ending at leaf node
-    def selection(self, player, root_board, previous_board_of_root):
-        current_node = self._BoardNode(player, root_board)
+    # selects a leaf node to expand (a node without children in the MCTS tree)
+    # input: self
+    # output: leaf node
+    def selection(self):
+        current_node = self._root_node
     
         while current_node.children:
             uct_values = [self._UCT(child) for child in current_node.children]
             current_node = current_node.children[uct_values.index(max(uct_values))]
-        
-        possible_next_boards = self._generate_valid_boards(current_node.player, current_node.board, previous_board_of_root)
-        
+            
+        if current_node.is_root:
+            possible_next_boards = self._generate_valid_boards(current_node.player, current_node.board, current_node.parent)
+        else:
+            possible_next_boards = self._generate_valid_boards(current_node.player, current_node.board, current_node.parent.board)
         next_player = 3 - current_node.player
 
         for board in possible_next_boards:
-            child_node = self._BoardNode(next_player, board, parent=current_node)
+            child_node = self._Node(next_player, board, current_node)
             current_node.children.append(child_node)
 
         return current_node
     
     # expands leaf node
+    # input: self, leaf node chosen during selection
+    # output: next child node to start simulation from
     def expansion(self, leaf_node):
-        if leaf_node.num_children_expanded == len(leaf_node.children) or self._is_terminal_state(leaf_node):
+        if leaf_node.num_children_simulated == len(leaf_node.children) or self._is_terminal_state(leaf_node):
             return None
         
-        unexplored_children = [child for child in leaf_node.children if not child.is_explored]
-        next_child = random.choice(unexplored_children)
-        next_child.is_explored = True
-        leaf_node.num_children_expanded += 1
+        unsimulated_children = [child for child in leaf_node.children if not child.simulated]
+        chosen_child = random.choice(unsimulated_children)
+        chosen_child.simulated = True
+        leaf_node.num_children_simulated += 1
         
-        return next_child
+        return chosen_child
     
     # simulates game from leaf node
     def simulation(self):
@@ -183,17 +193,29 @@ class MonteCarloTreeSearch:
         return False
 
 if __name__ == "__main__":
+    #instantiate MCTS agent
     agent_MCTS = MonteCarloTreeSearch("./input.txt")
-    player, current_board, previous_board = agent_MCTS.readInput()
-    print(f"Player: {player}\nBoard after agent's move:\n{previous_board}\nBoard after opponent's move:\n{current_board}\n")
     
-    leaf_node = agent_MCTS.selection(player, current_board, previous_board)
-    print("Leaf node board:\n")
-    for row in leaf_node.board: 
+    # read and print input
+    player, current_board, previous_board = agent_MCTS.readInput()
+    print(f"Player: {player}\nBoard after agent's move:")
+    for row in previous_board:
         print(row)
+    print("Board after opponent's move:")
+    for row in current_board:
+        print(row)
+    
+    # set root node for MCTS tree
+    agent_MCTS.set_root_node(player, current_board, previous_board)
+    
+    # for i in range(20):
+    leaf_node = agent_MCTS.selection()
     child_node = agent_MCTS.expansion(leaf_node)
-    #print details about the child node
-    print("Child node board:\n")
-    for row in child_node.board: 
+
+    print("\nLeaf node's board:")
+    for row in leaf_node.board:
         print(row)
-    print(f"parent node: {child_node.parent.identifier}\nplayer: {child_node.player}\nboard:{child_node.board}\n")
+    
+    print("\nChild node's board:")
+    for row in child_node.board:
+        print(row)
