cc15e62a4b4fdce7788b91e95e280d385f24fbcd
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2023-10-13 12:10:47 -0700

    feat: add root win - added a function to check if the root node won at end of sim

--- my_player3.py
+++ my_player3.py
@@ -17,8 +17,24 @@ class MonteCarloTreeSearch:
         self._root_node = None
         
     # sets the root node for the MCTS tree
+    # input: self, player number, current board layout, previous board layout
+    # Output none (modifies the root node)
     def set_root_node(self, player, current_board, previous_board):
         self._root_node = self._Node(player, current_board, previous_board)
+        
+    # reads & parses input file
+    # input: self
+    # output: color number agent is playing as, board layout after agent's last move, board layout after opponent's last move
+    def readInput(self):
+        with open(self._input_file_path, 'r') as file:
+            lines = [line.strip() for line in file.readlines()]
+
+        player = int(lines[0])
+        previous_board = [list(map(int, list(line))) for line in lines[1:6]]
+        current_board = [list(map(int, list(line))) for line in lines[6:11]]
+        file.close()
+
+        return player, current_board, previous_board
     
     # selects a leaf node to expand (a node without children in the MCTS tree)
     # input: self
@@ -58,7 +74,8 @@ class MonteCarloTreeSearch:
         while True:
             possible_next_boards = self._generate_valid_boards(current_node.player, current_node.board, current_node.previous_board)
             if not possible_next_boards:
-                return current_node
+                root_player_won = self._has_root_player_won(current_node.board)
+                return current_node, root_player_won
             
             next_board = random.choice(possible_next_boards)
             
@@ -86,20 +103,26 @@ class MonteCarloTreeSearch:
     # backpropagates result of simulation, updating each node on path
     def backpropagation(self):
         pass
+    
+    # determines if the root player has won the game
+    # input self, board layout at end of game
+    # output: True if root player has won, False otherwise
+    def _has_root_player_won(self, ending_board):
+        komi_value = 2.5
 
-    # reads & parses input file
-    # input: self
-    # output: color number agent is playing as, board layout after agent's last move, board layout after opponent's last move
-    def readInput(self):
-        with open(self._input_file_path, 'r') as file:
-            lines = [line.strip() for line in file.readlines()]
-
-        player = int(lines[0])
-        previous_board = [list(map(int, list(line))) for line in lines[1:6]]
-        current_board = [list(map(int, list(line))) for line in lines[6:11]]
-        file.close()
-
-        return player, current_board, previous_board
+        # Calculate scores for each player
+        player1_score = sum(row.count(1) for row in ending_board)
+        player2_score = sum(row.count(2) for row in ending_board) + komi_value
+        # print(f"\n\nPlayer 1 score: {player1_score} & Player 2 score: {player2_score}")
+        
+        if self._root_node.player == 1:
+            root_score = player1_score
+            opponent_score = player2_score
+        else:
+            root_score = player2_score
+            opponent_score = player1_score
+        
+        return root_score > opponent_score
         
     # generates all valid boards that can be reached from the given board by the given player
     # input: self, player, current board layout, previous board layout
@@ -200,7 +223,7 @@ if __name__ == "__main__":
     
     # read and print input
     player, current_board, previous_board = agent_MCTS.readInput()
-    print(f"Player: {player}\nBoard after agent's move:")
+    print(f"Agent Player: {player}\nBoard after agent's move:")
     for row in previous_board:
         print(row)
     print("\nBoard after opponent's move:")
@@ -213,11 +236,4 @@ if __name__ == "__main__":
     # for i in range(20):
     leaf_node = agent_MCTS.selection()
     child_node = agent_MCTS.expansion(leaf_node)
-    #print("\nStarting node's board for simulation:")
-    #for row in child_node.board:
-        #print(row)
-        
-    ending_node = agent_MCTS.simulation(child_node)
-    #print("\nEnding node's board after simulation:")
-    #for row in ending_node.board:
-        #print(row)
+    ending_node, root_player_won = agent_MCTS.simulation(child_node)
